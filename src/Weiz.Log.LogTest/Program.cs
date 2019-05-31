f4aceb9d97e56ee93919eac56fa3ff79779f24c9
weizhong1988 <weizhong1988@gmail.com>
2019-05-31 10:40:44 +0800

    首次提交

---
+++ src/Weiz.Log.LogTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weiz.Log.LogTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                try
+                {
+                    Logs.Instance.Info("测试测试");
+                    if (i % 1000 == 0)
+                    {
+                        int x = 0;
+                        x = 1 / x;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logs.Instance.Error(ex.Message + ex.StackTrace);
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
